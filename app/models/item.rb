edc222c950c916e16450bda8b210231c904cc898
Dimitri Bosch <dimitribosch@hotmail.com>
2018-02-17 16:26:43 +0100

    Fix navs counter badge issue

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
 
   scope :undone, -> { where(done: false) }
-  scope :before, ->(date) { where('date <= ?', date) }
+  scope :before, ->(date) { where('date < ?', date) }
   scope :of, ->(date) { where(date: date) }
   scope :tasks, -> { where(kind: "Task") }
 
